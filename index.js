9a225554a255614e5d6f06b466f98099513002a0
Anne Hastings <anne.miriam@gmail.com>
2023-11-05 17:32:31 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+const findMatching = (array, letter) => {
+    letter = letter.toLowerCase();
+    return array.filter(driver => driver.toLowerCase().startsWith(letter))
+};
+
+const fuzzyMatch = (array, string) => {
+    // string = string.slice(0, 2);
+    // return array.filter(driver => driver.slice(string))
+    string = string.toLowerCase();
+    return array.filter(driver => driver.slice(0,2).toLowerCase() === string)
+};
+
+const matchName = (array, name) => {
+    return array.filter(driver => driver.name === name);
+}
